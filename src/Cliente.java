ddc09053051b0c88b078306bef6bf3444700e154
Natalia Lacerda <natalia.lacerda@techcert.local>
2023-09-14 17:02:19 -0300

    first commit

---
+++ src/Cliente.java
@@ -0,0 +1,11 @@
+// Classe Cliente
+public class Cliente {
+    public static void main(String[] args) {
+
+        // Atributos
+        String nome;
+        String cpf;
+        String endereco;
+        String telefone;
+    }
+}
